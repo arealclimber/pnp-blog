72badd2059e8c132b745767803bc4620b2ac363e
arealclimber <luffulgence@gmail.com>
2025-02-16 04:48:33 +0800

    feat: 🎸 add formal projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,9 +7,33 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'Server Swarm',
+    description: `A clusters of servers that are connected to each other. Build the structure of prod and dev CI/CD pipeline.`,
+    // imgSrc: '/static/images/default-project.jpeg',
+    // href: '',
+  },
+  {
+    title: 'AI feature in English speaking',
+    description: `Multi-speaker voice recognition and LLM integration in English conversation.`,
+    // imgSrc: '/static/images/default-project.jpeg',
+    // href: '',
+  },
+  {
+    title: 'iSunFA',
+    description: `Database migration and accounting-related backend development.`,
+    // imgSrc: '/static/images/default-project.jpeg',
+    href: 'https://isunfa.com',
+  },
+  {
+    title: 'BAIFA',
+    description: `Build the frontend and API of blockchain explorer.`,
+    // imgSrc: '/static/images/default-project.jpeg',
+    href: 'https://baifa.io',
+  },
+  {
     title: 'TideBit DeFi',
-    description: `TideBit is a DeFi platform that allows you to earn interest on your crypto.`,
-    imgSrc: '/static/images/tidebit-defi.svg',
+    description: `Lead the frontend development of a Crypto DeFi platform.`,
+    // imgSrc: '/static/images/tidebit-defi.svg',
     href: 'https://tidebit-defi.com',
   },
   // {
