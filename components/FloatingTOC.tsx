b1ed0e8907d466e7979c9edb2e0e5627e1f1fd7e
arealclimber <luffulgence@gmail.com>
2025-03-02 15:11:22 +0800

    refactor: 💡 adjust the default toggle status and height of floa
    -ting toc

--- components/FloatingTOC.tsx
+++ components/FloatingTOC.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export interface TOCItem {
   value: string
@@ -13,6 +13,14 @@ interface FloatingTOCProps {
 
 export default function FloatingTOC({ toc }: FloatingTOCProps) {
   const [isCollapsed, setIsCollapsed] = useState(false)
+
+  // 在 client side 判斷偵測螢幕寬度
+  useEffect(() => {
+    // 使用 768px 為行動裝置斷點
+    if (window.innerWidth < 768) {
+      setIsCollapsed(true)
+    }
+  }, [])
 
   const toggleCollapse = () => {
     setIsCollapsed((prev) => !prev)
@@ -30,9 +38,9 @@ export default function FloatingTOC({ toc }: FloatingTOCProps) {
       <path
         d="M13 8L9 12M9 12L13 16M9 12H21M19.4845 7C17.8699 4.58803 15.1204 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C15.1204 21 17.8699 19.412 19.4845 17"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
@@ -49,15 +57,15 @@ export default function FloatingTOC({ toc }: FloatingTOCProps) {
       <path
         d="M11 16L15 12M15 12L11 8M15 12H3M4.51555 17C6.13007 19.412 8.87958 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C8.87958 3 6.13007 4.58803 4.51555 7"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   )
 
   return (
-    <nav className="fixed right-0 top-48 z-50 max-h-[80vh] max-w-[50vw] overflow-y-auto border border-gray-200 bg-white p-2 opacity-90 shadow-lg dark:border-gray-700 dark:bg-gray-900 md:max-w-[25vw]">
+    <nav className="fixed right-0 top-1/3 z-50 max-h-[80vh] max-w-[50vw] overflow-y-auto border border-gray-200 bg-white p-2 opacity-90 shadow-lg dark:border-gray-700 dark:bg-gray-900 md:max-w-[25vw]">
       <div className="flex items-center justify-between gap-3">
         <p className={`${isCollapsed ? 'hidden' : 'block text-start font-semibold'}`}>
           Table of Contents
@@ -71,7 +79,7 @@ export default function FloatingTOC({ toc }: FloatingTOCProps) {
       </div>
       <div className={`${isCollapsed ? 'hidden' : 'mt-3 flex flex-col gap-2'}`}>
         {!isCollapsed && (
-          <ul className="m-0 list-disc pl-2 text-gray-800 dark:text-gray-300">
+          <ul className="ml-2 list-disc pl-2 text-gray-800 dark:text-gray-300">
             {toc.map((item) => {
               // 若 url 尾端符合 -數字（例如 -4），則移除
               const cleanedUrl = item.url.replace(/-\d+$/, '')
